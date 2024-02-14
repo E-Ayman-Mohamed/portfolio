2cf7ff31a69dacfcaf9d8f09790c964af7198025
Ayman-Mohamed-abotaha <eng.ayman.mohamed.2001@gmail.com>
2024-02-14 13:24:56 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Profile from "./components/Profile/Profile";
+import Qualification from "./components/Qualification/Qualification";
+import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer/Footer";
+import Title from "./components/title/Title";
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="mainContainer">
+        <div className="row">
+          {" "}
+          <Header />
+        </div>
+
+        <div className="App">
+          <div id="home">
+            <div style={{ height: "7vh" }}></div>
+            <div className="row">
+              <div>
+                <Title />
+              </div>
+              <div className="col">
+                <Profile />
+              </div>
+              <div
+                className="col"
+                style={{ display: "grid", alignContent: "center" }}
+              >
+                <Qualification />
+              </div>
+            </div>
+          </div>
+          <hr></hr>
+          <div id="about" className="row">
+            <About />
+          </div>
+          <hr></hr>
+          <div id="projects" className="row">
+            <Projects></Projects>
+          </div>
+          <hr></hr>
+          <div id="contact" className="row">
+            <Footer></Footer>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
